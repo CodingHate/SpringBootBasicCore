bd3df09c394e320081efd503a5170acb643d0cce
hyunwoo <junghw86@naver.com>
2023-10-18 12:40:46 +0900

    study: chap2. 회원 도메인 실행과 테스트 : 3분 48초

--- src/main/java/com/example/springbootbsiccore/SpringbootBsicCoreApplication.java
+++ src/main/java/com/example/springbootbsiccore/SpringbootBsicCoreApplication.java
@@ -1,13 +1,25 @@
 package com.example.springbootbsiccore;
 
+import com.example.springbootbsiccore.member.Grade;
+import com.example.springbootbsiccore.member.Member;
+import com.example.springbootbsiccore.member.MemberService;
+import com.example.springbootbsiccore.member.MemberServiceImpl;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class SpringbootBsicCoreApplication {
 
-    public static void main(String[] args) {
-        SpringApplication.run(SpringbootBsicCoreApplication.class, args);
+    public static void main(String[] args)
+    {
+        MemberService memberService = new MemberServiceImpl();
+        Member member = new Member(1L, "memberA", Grade.MVP);
+        memberService.joing(member);
+
+        Member findMember = memberService.findMember(1L);
+        System.out.println("new member = " + member.getName());
+        System.out.println("find member = " + findMember.getName());
+//        SpringApplication.run(SpringbootBsicCoreApplication.class, args);
     }
 
 }
