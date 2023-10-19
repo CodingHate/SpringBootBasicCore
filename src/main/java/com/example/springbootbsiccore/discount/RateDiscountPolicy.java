da732ccc068c8550319cfe4510920558fe2188d0
hyunwoo <junghw86@naver.com>
2023-10-19 12:52:06 +0900

    chap03. 새로운 할인 정책 개발(완)

---
+++ src/main/java/com/example/springbootbsiccore/discount/RateDiscountPolicy.java
@@ -0,0 +1,19 @@
+package com.example.springbootbsiccore.discount;
+
+import com.example.springbootbsiccore.member.Grade;
+import com.example.springbootbsiccore.member.Member;
+
+public class RateDiscountPolicy implements DiscountPolicy{
+
+    private int discountPercent = 10;
+
+    @Override
+    public int discount(Member member, int price) {
+        if(member.getGread() == Grade.VIP){
+            return price * 10 / 100;
+        }
+        else{
+            return 0;
+        }
+    }
+}
