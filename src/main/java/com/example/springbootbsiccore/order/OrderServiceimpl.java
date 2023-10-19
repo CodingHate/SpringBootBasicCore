35f3d1a43eeefd77db97b1b7cd87f860ae0d298e
hyunwoo <junghw86@naver.com>
2023-10-20 08:44:15 +0900

    study: chap03, 새로운 할인 정책 적용과 문제점. DIP, OCP위반 상태이다. 이것이 왜 위반인지 에 대해서 설명을 들었다

--- src/main/java/com/example/springbootbsiccore/order/OrderServiceimpl.java
+++ src/main/java/com/example/springbootbsiccore/order/OrderServiceimpl.java
@@ -2,6 +2,7 @@ package com.example.springbootbsiccore.order;
 
 import com.example.springbootbsiccore.discount.DiscountPolicy;
 import com.example.springbootbsiccore.discount.FindDiscountPolicy;
+import com.example.springbootbsiccore.discount.RateDiscountPolicy;
 import com.example.springbootbsiccore.member.Member;
 import com.example.springbootbsiccore.member.MemberRepository;
 import com.example.springbootbsiccore.member.MemoryMemberRepository;
@@ -9,7 +10,9 @@ import com.example.springbootbsiccore.member.MemoryMemberRepository;
 public class OrderServiceimpl implements OrderService {
     
     private final MemberRepository memberRepository = new MemoryMemberRepository();
-    private final DiscountPolicy discountPolicy = new FindDiscountPolicy();
+    // private final DiscountPolicy discountPolicy = new FindDiscountPolicy();
+    // private final DiscountPolicy discountPolicy = new RateDiscountPolicy();
+    private DiscountPolicy discountPolicy;
     @Override
     public Order createOrder(Long memberId, String itemName, int itemPrice) {
         Member memeber = memberRepository.findById(memberId);
