40146f06e16f544b9bfcb1ea682ab645ca294f04
hyunwoo <junghw86@naver.com>
2023-10-23 22:21:20 +0900

    study: chap03 _ 스프링 빈 조회-기본 (완( 조회하는 방법에 대해서 테스트 코드로 만들어 보았다

---
+++ src/test/java/com/example/springbootbsiccore/beanfind/ApplicationContextBasicFindTest.java
@@ -0,0 +1,49 @@
+package com.example.springbootbsiccore.beanfind;
+
+import com.example.springbootbsiccore.AppConfig;
+import com.example.springbootbsiccore.member.MemberService;
+import com.example.springbootbsiccore.member.MemberServiceImpl;
+import org.assertj.core.api.Assertions;
+import org.junit.Assert;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.NoSuchBeanDefinitionException;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+public class ApplicationContextBasicFindTest {
+
+    AnnotationConfigApplicationContext ac = new AnnotationConfigApplicationContext(AppConfig.class);
+
+    @Test
+    @DisplayName("빈 이름으로 조회")
+    void findBeanByName(){
+        MemberService memberService = ac.getBean("memberService", MemberService.class);
+//        System.out.println("memberService = " + memberService);
+//        System.out.println("memeberService.getClass() = " + memberService.getClass());
+        Assertions.assertThat(memberService).isInstanceOf(MemberServiceImpl.class);
+    }
+
+    @Test
+    @DisplayName("이름 없이 타입으로만 조회")
+    void findBeanType(){
+        MemberService memberService = ac.getBean(MemberService.class);
+//        System.out.println("memberService = " + memberService);
+//        System.out.println("memeberService.getClass() = " + memberService.getClass());
+        Assertions.assertThat(memberService).isInstanceOf(MemberServiceImpl.class);
+    }
+
+    @Test
+    @DisplayName("구체 타입으로 조회")
+    void findBeanByName2(){
+        MemberService memberService = ac.getBean("memberService",MemberService.class);
+        Assertions.assertThat(memberService).isInstanceOf(MemberServiceImpl.class);
+    }
+
+    @Test
+    @DisplayName("빈 이름으로 조회X")
+    void fidnBeanByName(){
+        Assert.assertThrows(NoSuchBeanDefinitionException.class,
+                ()->ac.getBean("XXXX", MemberService.class));
+    }
+}
+
