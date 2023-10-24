9820f5b704f4686c16a9a2bdd001ebcce9ae2ee3
hyunwoo <junghw86@naver.com>
2023-10-25 08:16:42 +0900

    study: chap05 @Configuration과 싱글톤 Configuration에 등록 된 Bean은 어떠한 경우라도 Singleton이 보장 되어 있다.

---
+++ src/test/java/com/example/springbootbsiccore/singletone/ConfigurationSingletonTest.java
@@ -0,0 +1,32 @@
+package com.example.springbootbsiccore.singletone;
+
+import com.example.springbootbsiccore.AppConfig;
+import com.example.springbootbsiccore.member.MemberRepository;
+import com.example.springbootbsiccore.member.MemberServiceImpl;
+import com.example.springbootbsiccore.order.OrderServiceimpl;
+import org.assertj.core.api.Assertions;
+import org.junit.Test;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+public class ConfigurationSingletonTest {
+
+    @Test
+    public void configurationTest(){
+        ApplicationContext ac = new AnnotationConfigApplicationContext(AppConfig.class);
+
+        MemberServiceImpl memberService = ac.getBean("memberService", MemberServiceImpl.class);
+        OrderServiceimpl orderService = ac.getBean("orderService", OrderServiceimpl.class);
+        MemberRepository memberRepository = ac.getBean("memberRepository", MemberRepository.class);
+
+        MemberRepository memberRepository1 = memberService.getMemberRepository();
+        MemberRepository memberRepository2 = orderService.getMemberRepository();
+
+        System.out.println("memberService -> memberRepository = " + memberRepository1);
+        System.out.println("orderService -> memberRepository = " + memberRepository2);
+        System.out.println("memberRepository = " + memberRepository);
+
+        Assertions.assertThat(memberService.getMemberRepository()).isSameAs(memberRepository);
+        Assertions.assertThat(orderService.getMemberRepository()).isSameAs(memberRepository);
+    }
+}
