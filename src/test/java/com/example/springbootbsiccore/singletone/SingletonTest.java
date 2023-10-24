ec1d89721f51f91920da304319991805b32d65c4
hyunwoo <junghw86@naver.com>
2023-10-25 07:39:04 +0900

    study: chap05. 싱글톤패 싱글톤패턴에 대해서 확인 하였다. 알고 있는 내용이다. 그래도 q /q
    asdfasdfasdf
    adsfasdfqzxcvzxcv
    !
    /
    /q
    !
    q
    quit
    /quit
    exist
    exit
    asdfasdf

--- src/test/java/com/example/springbootbsiccore/singletone/SingletonTest.java
+++ src/test/java/com/example/springbootbsiccore/singletone/SingletonTest.java
@@ -6,11 +6,14 @@ import com.example.springbootbsiccore.member.MemberServiceImpl;
 import org.assertj.core.api.Assertions;
 import org.junit.Test;
 import org.junit.jupiter.api.DisplayName;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.support.GenericXmlApplicationContext;
 
 public class SingletonTest {
 
     @Test
-    @DisplayName("")
+    @DisplayName("스프리이 없는 순수한 DI 컨테이너")
     public void pureContainer(){
         AppConfig appConfig = new AppConfig();
 
@@ -37,5 +40,21 @@ public class SingletonTest {
         System.out.println("singletonService02 = " + singletonService02);
 
         Assertions.assertThat(singletonService01).isSameAs(singletonService02);
+    }
+
+    @Test
+    @DisplayName("스프링 컨테이너와 싱글톤")
+    public void springContainer(){
+        //        AppConfig appConfig = new AppConfig();
+        ApplicationContext ac = new AnnotationConfigApplicationContext(AppConfig.class);
+        // 1. 조회
+        MemberService memberService01 = ac.getBean("memberService", MemberService.class);
+        MemberService memberService02 = ac.getBean("memberService", MemberService.class);
+
+        //참조값이 다른 것을 확인
+        System.out.println("memeberService01 = " + memberService01);
+        System.out.println("memeberService02 = " + memberService02);
+
+        Assertions.assertThat(memberService01).isSameAs(memberService02);
     }
 }
