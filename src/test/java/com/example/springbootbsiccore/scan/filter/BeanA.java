f06362bc077d4ad940f2296d559108e04f5e0e7f
hyunwoo <junghw86@naver.com>
2023-10-26 10:17:24 +0900

    study: chap06: 필터

---
+++ src/test/java/com/example/springbootbsiccore/scan/filter/BeanA.java
@@ -0,0 +1,5 @@
+package com.example.springbootbsiccore.scan.filter;
+
+@MyIncludeComponent
+public class BeanA {
+}
---
+++ src/test/java/com/example/springbootbsiccore/scan/filter/BeanB.java
@@ -0,0 +1,5 @@
+package com.example.springbootbsiccore.scan.filter;
+
+@MyExcludeComponent
+public class BeanB {
+}
