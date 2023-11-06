9e06da52d72b51eb888b8313026eea151000752f
hyunwoo <junghw86@naver.com>
2023-11-07 08:29:46 +0900

    study: 프로토타입 스코프- 싱글톤 빈과 함께 사용시 Provider로 문제 해결

--- src/test/java/com/example/springbootbsiccore/scope/SingletonWithPrototypeTest1.java
+++ src/test/java/com/example/springbootbsiccore/scope/SingletonWithPrototypeTest1.java
@@ -4,9 +4,12 @@ import jakarta.annotation.PostConstruct;
 import jakarta.annotation.PreDestroy;
 import org.assertj.core.api.Assertions;
 import org.junit.Test;
+import org.springframework.beans.factory.ObjectProvider;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import org.springframework.context.annotation.Scope;
+
+import javax.inject.Provider;
 
 import static org.assertj.core.api.Assertions.*;
 
@@ -37,22 +40,26 @@ public class SingletonWithPrototypeTest1 {
 
         ClientBean clientBean2 = ac.getBean(ClientBean.class);
         int count2 = clientBean2.logic();
-        assertThat(count2).isEqualTo(2); // prototype 호출시 미리 주입된것을 가져오므로 초기값이 기존 수행된 값으로 가지게 된다.
+        assertThat(count2).isEqualTo(1); // prototype 호출시 미리 주입된것을 가져오므로 초기값이 기존 수행된 값으로 가지게 된다.
 
     }
 
     @Scope("singleton")
     static class ClientBean{
-        private final PrototpyeBean prototpyeBean; // 생성시점에 주입, 미리 기존 주입이 되어 있으면 그대로 사용하게 된다. 따라서 싱글톤으로 사용시 문제가 발생될수 있다.
+//        private final PrototpyeBean prototpyeBean; // 생성시점에 주입, 미리 기존 주입이 되어 있으면 그대로 사용하게 된다. 따라서 싱글톤으로 사용시 문제가 발생될수 있다.
 
         @Autowired
-        public ClientBean(PrototpyeBean prototpyeBean) {
-            this.prototpyeBean = prototpyeBean;
-        }
+        private Provider<PrototpyeBean> prototypeBeanProvider;
+
+//        @Autowired
+//        public ClientBean(PrototpyeBean prototpyeBean) {
+//            this.prototpyeBean = prototpyeBean;
+//        }
 
         public int logic(){
-            prototpyeBean.addCount();
-            int count = prototpyeBean.getCount();
+            PrototpyeBean prototypeBean = prototypeBeanProvider.get();
+            prototypeBean.addCount();
+            int count = prototypeBean.getCount();
             return count;
         }
     }
